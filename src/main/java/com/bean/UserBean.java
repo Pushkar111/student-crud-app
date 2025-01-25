49a1c9f78632d0fc37347e4b11bfe72ddf598029
Pushkar Modi <116958355+Pushkar111@users.noreply.github.com>
2025-01-26 03:30:29 +0530

    Add UserBean class to represent user data

---
+++ src/main/java/com/bean/UserBean.java
@@ -0,0 +1,61 @@
+package com.bean;
+
+public class UserBean
+{
+	private int id;
+	private String full_name;
+	private String email;
+	private String password;
+	
+	public UserBean() {
+		
+	}
+	
+	public UserBean(int id, String full_name, String email, String password)
+	{
+		this.id = id;
+		this.full_name = full_name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public String getFull_name()
+	{
+		return full_name;
+	}
+	
+	public void setFull_name(String full_name)
+	{
+		this.full_name = full_name;
+	}
+	
+	public String getEmail()
+	{
+		return email;
+	}
+	
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
+	
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+}
